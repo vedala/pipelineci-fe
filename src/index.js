3b176912caf52a74d1c32aee326cda5ce77ee220
Kalyan Vedala <svedala23@gmail.com>
2024-05-19 06:58:53 -0700

    Wrap app in Auth0Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
