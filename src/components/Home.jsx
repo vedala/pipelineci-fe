90dfc7c52795d247d47541ebb086710f4a1f8ff9
Kalyan Vedala <svedala23@gmail.com>
2024-05-19 08:42:57 -0700

    Add link to CreateOrganization to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+import LogoutButton from "./LogoutButton";
+
 function Home() {
   return (
-    <h1>PipelineCI Home Page</h1>
+    <>
+      <LogoutButton />
+      <h1>PipelineCI Home Page</h1>
+      <Link to="/create-organization">Create Organization</Link>
+    </>
   );
 }
 
