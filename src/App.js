2f99f08fc6165093d36b862810e3304cf541ee5a
Kalyan Vedala <svedala23@gmail.com>
2024-05-19 06:16:06 -0700

    Render login, logout and signup buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Landing from './components/Landing';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import SignupButton from './components/Signup';
 
 function App() {
   return (
     <div className="App">
+      <LoginButton /> <SignupButton />
+      <LogoutButton />
       <Landing />
     </div>
   );
