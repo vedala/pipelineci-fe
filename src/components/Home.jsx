b4fc6f957ef456188bf168dace35eac7dca884e8
Kalyan Vedala <svedala23@gmail.com>
2024-07-05 12:11:06 -0700

    Use useAuth0 to get user object, display user name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "./LogoutButton";
 
 function Home() {
+  const { user } = useAuth0();
+
   return (
     <>
       <h1>PipelineCI Home Page</h1>
+      <p>Logged in as: { user.name }</p>
       <Link to="/create-organization">Create Organization</Link>
     </>
   );
