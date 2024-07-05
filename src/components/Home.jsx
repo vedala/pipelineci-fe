ab5d1f0cb4551be9ecc2357c4ecb93b7e1edcfc1
Kalyan Vedala <svedala23@gmail.com>
2024-07-05 12:33:33 -0700

    Get and display organizations - work in progress

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 import LogoutButton from "./LogoutButton";
+
+const PIPELINECI_API_URL = process.env.REACT_APP_PIPELINECI_API_URL;
 
 function Home() {
   const { user } = useAuth0();
+console.log("user=", user);
+
+  const [organizations, setOrganizations] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      axios.get(`${PIPELINECI_API_URL}/organizations?user=${user.name}`)
+      .then(res => {
+        const orgData = res.data;
+        setOrganizations(orgData);
+      })
+      .catch(err => { console.log(err); throw err; });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
