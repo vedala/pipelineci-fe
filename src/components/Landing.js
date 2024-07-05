0942b353132f1fb894952f810850ed189de6b781
Kalyan Vedala <svedala23@gmail.com>
2024-07-05 11:23:16 -0700

    Link button to home page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Landing() {
   return (
     <>
       <h1>Welcome to PipelineCI</h1>
-      <button>Go to Application</button>
+      <Link to="/home">
+        <button>Go to Application</button>
+      </Link>
     </>
   );
 }
