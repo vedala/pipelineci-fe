7423f2c74efe7c90028d72f3a81a3dc9d58abeaa
Kalyan Vedala <svedala23@gmail.com>
2025-01-10 17:23:41 -0800

    Fetch data and render runs list

--- src/components/Runs.jsx
+++ src/components/Runs.jsx
@@ -1,11 +1,64 @@
+import { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const PIPELINECI_API_URL = process.env.REACT_APP_PIPELINECI_API_URL;
 
 function Runs() {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const [runs, setRuns] = useState([]);
+
   const { projectId } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      const token = await getAccessTokenSilently();
+
+      let headersObj;
+      if (process.env.NODE_ENV === "production") {
+        headersObj = {
+          'Authorization': `Bearer ${token}`,
+        }
+      } else {
+        headersObj = {
+          'Authorization': `Bearer ${token}`,
+          'ngrok-skip-browser-warning': '1',
+        }
+      }
+
+      await axios.get(`${PIPELINECI_API_URL}/runs?projectId=${projectId}`, {
+        headers: headersObj
+      })
+      .then(res => {
+        const runsData = res.data;
+        setRuns(runsData);
+      })
+      .catch(err => { console.log(err); throw err; });
+    }
+
+    fetchData();
+  }, [runs, getAccessTokenSilently, projectId]);
+
+  const runsList = runs.map((run) => {
+    return (
+      <li key={run.id}>
+        <span>Id: {run.id}</span>
+        <span>SHA: {run.sha}</span>
+        <span>Branch: {run.branch}</span>
+      </li>
+    )
+  });
+
   return (
     <>
       <h1>Runs Home</h1>
       <h2>Project: {projectId}</h2>
+      <label>
+        Runs:
+        <ul>{runsList}</ul>
+      </label>
     </>
   );
 }
