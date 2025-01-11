bf8ddbdd98e18d4db7cad8981071b26fd1d7ba56
Kalyan Vedala <svedala23@gmail.com>
2025-01-10 16:10:51 -0800

    Fetch and render project list

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,8 @@ function Projects() {
   const { user, getAccessTokenSilently } = useAuth0();
 
   const [organizations, setOrganizations] = useState([]);
-  const [selectedOrg,   setSelectedOrg]   = useState("");
+  const [selectedOrg,   setSelectedOrg]   = useState({key: "", value: ""});
+  const [projects,      setProjects]      = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -32,19 +33,64 @@ function Projects() {
       .then(res => {
         const orgData = res.data;
         setOrganizations(orgData);
-        setSelectedOrg(orgData[0].name);
+        setSelectedOrg({key: orgData[0].id, value: orgData[0].value});
       })
       .catch(err => { console.log(err); throw err; });
     }
+
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleOrgChange = async (e) => {
+    const selectedObject = {
+      key: e.target.options[e.target.selectedIndex].dataset.key,
+      value: e.target.value
+    };
+
+    setSelectedOrg(selectedObject);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const token = await getAccessTokenSilently();
+
+      let headersObj;
+      if (process.env.NODE_ENV === "production") {
+        headersObj = {
+          'Authorization': `Bearer ${token}`,
+        }
+      } else {
+        headersObj = {
+          'Authorization': `Bearer ${token}`,
+          'ngrok-skip-browser-warning': '1',
+        }
+      }
+
+      await axios.get(`${PIPELINECI_API_URL}/projects?orgId=${selectedOrg.key}`, {
+        headers: headersObj
+      })
+      .then(res => {
+        const projectsData = res.data;
+        setProjects(projectsData);
+      })
+      .catch(err => { console.log(err); throw err; });
+    }
+
+    fetchData();
+  }, [selectedOrg, getAccessTokenSilently]);
+
   const orgOptionList = organizations.map((organization) => {
     return (
-      <option key={organization.id} value={organization.name}>{organization.name}</option>
+      <option key={organization.id} data-key={organization.id} value={organization.name}>{organization.name}</option>
     );
   });
+
+  const projectList = projects.map((project) => {
+    return (
+      <li>{project.name}</li>
+    )
+  })
 
   return (
     <>
@@ -53,13 +99,17 @@ function Projects() {
       <label>
         Select an organization:
         <select
-          value={selectedOrg}
-          onChange={e => setSelectedOrg(e.target.value)}
+          value={selectedOrg.value}
+          onChange={handleOrgChange}
         >
           {orgOptionList}
         </select>
       </label>
-      <p>Selected Org: {selectedOrg}</p>
+      <p>Selected Org: {selectedOrg.value}</p>
+      <label>
+        Organization's projects:
+        <ul>{projectList}</ul>
+      </label>
     </>
   );
 }
